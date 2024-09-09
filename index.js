6ae2872e43218a1e9a4e80f9d7da617f6c98df00
srishtisingh <62917762+srishtisingh0508@users.noreply.github.com>
2024-09-08 20:40:40 -0700

    test

---
+++ index.js
@@ -0,0 +1,10 @@
+// Simple script to smooth scroll to sections
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
